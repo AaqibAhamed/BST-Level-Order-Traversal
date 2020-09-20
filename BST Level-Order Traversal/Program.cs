56330cf1f73d16c58b70d8d91cea64696e092d70
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-20 15:15:18 +0530

    Add LevelOrder

--- BST Level-Order Traversal/Program.cs
+++ BST Level-Order Traversal/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BST_Level_Order_Traversal
 {
     class Program
     {
-        static void Main(string[] args)
+        static void levelOrder(Node root)
         {
-            Console.WriteLine("Hello World!");
+            Node temp = root;
+
+            Queue<Node> nodeQueue = new Queue<Node>();
+
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+            }
+        }
+
+        static Node Insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = Insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = Insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = Insert(root, data);
+            }
+            levelOrder(root);
+
         }
     }
 }
